205a69e1fe908ad607187381e57d581d9eb71690
ADMIN <ADMIN@DESKTOP-02D6639>
2019-06-08 15:39:34 +0530

    first

---
+++ src/main/java/com/app/SprinprofileApplication.java
@@ -0,0 +1,13 @@
+package com.app;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SprinprofileApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SprinprofileApplication.class, args);
+	}
+
+}
